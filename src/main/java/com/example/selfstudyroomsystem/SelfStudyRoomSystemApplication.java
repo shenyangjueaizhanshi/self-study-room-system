8145b817b0ce8b881da7d9253fc3522a86326018
李瑞文 <1353122328@qq.com>
2023-02-15 09:13:30 +0800

    Initial commit

---
+++ src/main/java/com/example/selfstudyroomsystem/SelfStudyRoomSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.selfstudyroomsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfStudyRoomSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfStudyRoomSystemApplication.class, args);
+    }
+
+}
