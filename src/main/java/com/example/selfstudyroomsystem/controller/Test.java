2cba78bd7e4f2771209e5794fb3a464ec87092d9
李瑞文 <1353122328@qq.com>
2023-03-02 16:45:40 +0800

    规范Api参数

--- src/main/java/com/example/selfstudyroomsystem/controller/Test.java
+++ src/main/java/com/example/selfstudyroomsystem/controller/Test.java
@@ -2,18 +2,20 @@ package com.example.selfstudyroomsystem.controller;
 
 import com.example.selfstudyroomsystem.dao.TestDAO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(value = "测试")
+@Api(tags = "测试控制器")
 @RequestMapping("/test")
 @RestController
 public class Test {
     @Autowired
     private TestDAO  testDAO;
+    @ApiOperation(value = "测试接口")
     @PostMapping("/t")
     public String test(@RequestBody com.example.selfstudyroomsystem.bean.Test test){
        testDAO.insert(test);
